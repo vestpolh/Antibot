63a706c44ece03ed2ebce8c55327cab58cd68731
ivan.khloptsev <ivan.khloptsev@SHEVELEV-PC>
2023-05-04 17:54:53 +0300

    first commit

---
+++ Antibot.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Antibot.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
